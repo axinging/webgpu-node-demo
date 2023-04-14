7080f7e69fc756bda025be9ce529badd572de643
Xu Xing <xing.xu@intel.com>
2023-04-14 20:20:08 +0800

    Add tfjs on external node

---
+++ tfjs-on-external-node/README.md
@@ -0,0 +1,12 @@
+webgpu-node supports running webgpu and TensorflowJS(WIP) on NodeJS.
+
+## Building: 
+1. Build dawn.node
+https://source.chromium.org/chromium/chromium/src/+/main:third_party/dawn/src/dawn/node/README.md
+(Only tried Ubuntu/Linux)
+
+2. npm install
+3. copy dawn.node to build/folder.
+4. cp tf-backend-webgpu.node.js node_modules/@tensorflow/tfjs-backend-webgpu/dist/tf-backend-webgpu.node.js 
+5. node .
+
---
+++ tfjs-on-external-node/dawn.js
@@ -0,0 +1,38 @@
+const gpuProviderModule = require('bindings')('dawn');
+// const tf = require('@tensorflow/tfjs');
+// const tfwebgl = require('@tensorflow/tfjs-backend-webgl')
+const tfwebgpu = require('@tensorflow/tfjs-backend-webgpu');
+const tf = require('@tensorflow/tfjs-core');
+console.log(tfwebgpu);
+console.log(tfwebgpu.webgpu_util.isWebGPUSupported());
+
+
+console.log(tf.engine().backendNames());
+let gpuProvider = null;
+function setGPUProvider() {
+  const gpuProviderFlags = ['disable-dawn-features=disallow_unsafe_apis'];
+  gpuProvider = () => gpuProviderModule.create(gpuProviderFlags);
+}
+let gpuImpl = null;
+function getGPU() {
+  if (gpuImpl) {
+    return gpuImpl;
+  }
+  gpuImpl = gpuProvider();
+  return gpuImpl;
+}
+
+(async function main() {
+  setGPUProvider();
+  const GPU = getGPU();
+  const adapter = await GPU.requestAdapter();
+  const device = await adapter.requestDevice();
+  tf.env().set('WEBGPU_CPU_FORWARD', false);
+  await tf.setBackend('webgpu');
+  await tf.ready();
+
+  const a = tf.tensor1d([1, 2, 3, 4]);
+  const b = tf.tensor1d([10, 20, 30, 40]);
+  const result = tf.add(a, b);
+  console.log(await result.data());
+})();
