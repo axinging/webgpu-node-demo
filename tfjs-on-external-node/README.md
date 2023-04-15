e3d95e85553dc9a3ff18da04e2604400cf6c3937
Xu Xing <xing.xu@intel.com>
2023-04-15 17:23:12 +0800

    Nit

--- tfjs-on-external-node/README.md
+++ tfjs-on-external-node/README.md
@@ -5,7 +5,25 @@ webgpu-node supports running webgpu and TensorflowJS(WIP) on NodeJS.
 https://source.chromium.org/chromium/chromium/src/+/main:third_party/dawn/src/dawn/node/README.md
 (Only tried Ubuntu/Linux)
 
-2. Run
+2. Build tfjs
+
+```
+git clone https://github.com/axinging/tfjs.git -b tfjswebgpu_on_node
+cd  tfjs
+git checkout master
+git branch -D tfjswebgpu_on_node
+git pull --all
+git checkout tfjswebgpu_on_node
+yarn
+yarn bazel clean
+cd link-package && yarn build-deps-for tfjs-backend-webgpu tfjs-core tfjs-backend-cpu  tfjs-backend-webgl tfjs-converter
+cd ../tfjs-backend-webgpu && rm -fr node_modules && yarn && yarn build-npm
+yarn --cwd .. bazel build //tfjs-backend-webgpu/src:tests
+ln -s ../dist/bin/tfjs-backend-webgpu/src/tests.ts tests
+yarn && yarn build-npm
+```
+
+3. Run
 npm install
 mkdir build
 cp dawn.node ./build
--- tfjs-on-external-node/dawn.js
+++ tfjs-on-external-node/dawn.js
@@ -1,32 +1,10 @@
 const gpuProviderModule = require('bindings')('dawn');
-// const tf = require('@tensorflow/tfjs');
-// const tfwebgl = require('@tensorflow/tfjs-backend-webgl')
 const tfwebgpu = require('@tensorflow/tfjs-backend-webgpu');
 const tf = require('@tensorflow/tfjs-core');
-console.log(tfwebgpu);
-console.log(tfwebgpu.webgpu_util.isWebGPUSupported());
-
-console.log(tf.engine().backendNames());
-let gpuProvider = null;
-function setGPUProvider() {
-  const gpuProviderFlags = ['disable-dawn-features=disallow_unsafe_apis'];
-  gpuProvider = () => gpuProviderModule.create(gpuProviderFlags);
-}
-let gpuImpl = null;
-function getGPU() {
-  if (gpuImpl) {
-    return gpuImpl;
-  }
-  gpuImpl = gpuProvider();
-  return gpuImpl;
-}
 
 (async function main() {
-  setGPUProvider();
-  const GPU = getGPU();
-  const adapter = await GPU.requestAdapter();
-  const device = await adapter.requestDevice();
   tf.env().set('WEBGPU_CPU_FORWARD', false);
+  tf.env().set('WEBGPU_PRINT_SHADER', 'binary');
   await tf.setBackend('webgpu');
   await tf.ready();
 
